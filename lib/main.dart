b9731d9812636f0fb7e7aa02f45ae35256850b6a
brenor2 <rbrenorios@gmail.com>
2018-04-06 17:45:31 -0300

    first screen ready, without animations.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(new ProjectWd());
 
@@ -18,36 +19,65 @@ class ProjectWd extends StatelessWidget {
 class BackgroundWidget extends StatelessWidget {
   @override
     Widget build(BuildContext context) {
-      var assetsImage = new AssetImage('assets/bg.png');
+      var bgAssetImage = new AssetImage('assets/bg.png');
+      var name         = new NameWidget();
+      var button       = new LoginButtonWidget();
+
       var background = new Container(
-        child: new Image(
-          image: assetsImage
-          ),
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            fit: BoxFit.cover,
+            image: bgAssetImage,
+         ),
+        ),
       );
 
-      var button = new LoginButtonWidget();
-    
-       return new Stack(
-        children: <Widget>[background, button],
+      var column = new Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          new Expanded(child: name),
+          new Padding(child: button, padding: new EdgeInsets.only(bottom: 50.0),)
+      ],);
+      return new Stack(
+        children: <Widget>[background, column],
       );
     } 
+}
+
+class NameWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var namesAssetImage = new AssetImage('assets/names.png');
+
+    var name = new Container(
+        child: new Center(
+          child: new Image(
+            image: namesAssetImage
+          ,)
+        ),
+    );
+
+    return name;
+  }
 }
 
 class LoginButtonWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Container(
-        width: 249.0,
-        height: 46.0,
-        decoration: new BoxDecoration(
-          color: const Color.fromRGBO(128, 41, 41, 78.0),
-          borderRadius: new BorderRadius.all(const Radius.circular(50.0))
-        ),
-        child: new FlatButton(
-          child: new Text("Login", style: new TextStyle(fontSize: 24.0, color: Colors.white)),
-          onPressed: null,
-        ),
-      ));
+    var button = new Container(
+      width: 249.0,
+      height: 46.0,
+      decoration: new BoxDecoration(
+        color: const Color.fromRGBO(128, 41, 41, 78.0),
+        borderRadius: new BorderRadius.all(const Radius.circular(50.0))
+      ),
+      child: new FlatButton(
+        child: new Text("Login", style: new TextStyle(fontSize: 24.0, color: Colors.white)),
+        onPressed: null,
+      ),
+    );
+      
+      return button;
   }
 }
